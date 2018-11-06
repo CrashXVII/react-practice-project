bb72f347191e670ddb360497a7faa54df61d12db
John Barhorst <crashdsinc@comcast.net>
2018-11-05 21:05:25 -0600

    specified eslint parser to fix '=' error notice, changed api items in state

--- src/MovieList.js
+++ src/MovieList.js
@@ -5,11 +5,9 @@ export default class MovieList extends Component {
     super(props);
     this.state = {
       movielist: [],
-      APIConfig: {
-        key: 'api_key=bc1fea1a843eab396d852f9f7d26a75e',
-        base: 'https://api.themoviedb.org/3/',
-        searchpath: 'movie/popular?',
-      },
+      key: 'api_key=bc1fea1a843eab396d852f9f7d26a75e',
+      base: 'https://api.themoviedb.org/3/',
+      searchpath: 'movie/popular?',
     };
   }
 
@@ -23,18 +21,13 @@ export default class MovieList extends Component {
 
   handleSearchSelect = (e) => {
     this.setState({
-      APIConfig: {
-        key: 'api_key=bc1fea1a843eab396d852f9f7d26a75e',
-        base: 'https://api.themoviedb.org/3/',
-        searchpath: e.target.value,
-      },
+      searchpath: e.target.value,
     });
     this.getMovies();
   }
 
   getMovies = async () => {
-    const { APIConfig } = this.state;
-    const { key, base, searchpath } = APIConfig;
+    const { key, base, searchpath } = this.state;
     const response = await fetch(`${base}${searchpath}${key}`);
     const data = await response.json();
     const movielist = await data.results;
@@ -49,10 +42,10 @@ export default class MovieList extends Component {
       <div>
         { movielist.map(movie => (<h1 key={movie.id}>{ movie.title }</h1>))}
         <select name="search-select" id="search-select" onChange={this.handleSearchSelect}>
+          <option value="movie/popular?">Popular</option>
           <option value="movie/upcoming?">Upcoming</option>
           <option value="movie/top_rated?">Top Rated</option>
           <option value="movie/now_playing?">Now Playing</option>
-          <option value="movie/popular?">Popular</option>
         </select>
       </div>
     );
