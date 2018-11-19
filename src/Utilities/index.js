a88141993904380cc42909c829d2eac98ce9d395
John Barhorst <crashdsinc@comcast.net>
2018-11-18 20:41:10 -0600

    started fleshing out Utilities

---
+++ src/Utilities/index.js
@@ -0,0 +1,6 @@
+import elevation from './elevation';
+import transition from './transition';
+
+export * from './position';
+
+export { elevation, transition };
---
+++ src/Utilities/position.js
@@ -0,0 +1,7 @@
+export const absolute = ({ y = 'top', x = 'left' }) => `
+  position: absolute;
+  ${y}: 0;
+  ${x}: 0;
+`;
+
+export default { absolute };
